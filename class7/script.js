e96dd9972a9c3a9948efb8396f6630db4c80c6bc
Swapnil Sharma <nilinswap@gmail.com>
2023-04-29 11:38:12 +0530

    class 7

--- class7/script.js
+++ class7/script.js
@@ -6,6 +6,25 @@
 // This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+function setWeather(res) {
+  fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${res.lat}&longitude=${res.long}&current_weather=true&forecast_days=1&timezone=auto`
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      console.log("res", res);
+
+      document.getElementById("temperature").innerHTML =
+        res.current_weather.temperature;
+      document.getElementById("windSpeed").innerHTML =
+        res.current_weather.windspeed;
+      document.getElementById("windDirection").innerHTML =
+        res.current_weather.winddirection;
+      document.getElementById("elevation").innerHTML = res.elevation;
+      return res;
+    });
+}
 function initMap() {
   const input = document.getElementById("pac-input");
 
@@ -16,6 +35,10 @@ function initMap() {
   autocomplete.addListener("place_changed", () => {
     const place = autocomplete.getPlace();
     console.log(place.geometry.location.lat(), place.geometry.location.lng());
+    setWeather({
+      lat: place.geometry.location.lat(),
+      long: place.geometry.location.lng(),
+    });
 
     if (!place.geometry || !place.geometry.location) {
       // User entered the name of a Place that was not suggested and
