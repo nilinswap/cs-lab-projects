7896776d83adc2f3df386d675e2743e65554013e
Swapnil Sharma <nilinswap@gmail.com>
2023-04-29 14:01:50 +0530

    class 6 50 done

--- class6/utils.js
+++ class6/utils.js
@@ -1,15 +1,8 @@
-var scrollTo = function (identifier, speed) {
-  $("html, body").animate(
-    {
-      scrollTop: $(identifier).offset().top,
-    },
-    speed || 1000
-  );
+const unhide = function (id) {
+  document.getElementById(id).className = "";
 };
-
 export var hello = function () {
-  $("#hello").slideDown("slow");
-  scrollTo("#section_hello");
+  unhide("hello");
   responsiveVoice.speak("Hello this is Swapnil, How are you doing today?");
 };
 
@@ -18,37 +11,22 @@ export var helloFunction = function (name) {
 };
 
 export var showFlickr = function (tag) {
-  $("#flickrGallery").show();
-  $("#flickrLoader p")
-    .text("Searching for " + tag)
-    .fadeIn("fast");
+  unhide("flickrGallery");
+  // TODO: do a loader
   fetch(
     `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a828a6571bb4f0ff8890f7a386d61975&sort=interestingness-desc&per_page=9&format=json&nojsoncallback=1&tags=${tag}`
   )
     .then((res) => res.json())
     .then((res) => jsonFlickrApi(res));
-
-  scrollTo("#section_image_search");
 };
 
 var jsonFlickrApi = function (results) {
-  $("#flickrLoader p").fadeOut("slow");
   var photos = results.photos.photo;
-  $.each(photos, function (index, photo) {
-    $(document.createElement("img"))
-      .attr({
-        src:
-          "//farm" +
-          photo.farm +
-          ".staticflickr.com/" +
-          photo.server +
-          "/" +
-          photo.id +
-          "_" +
-          photo.secret +
-          "_s.jpg",
-      })
-      .addClass("flickrGallery")
-      .appendTo(flickrGallery);
+  console.log(photos, "photos");
+  photos.forEach((photo) => {
+    var newImage = document.createElement("img");
+    newImage.src = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_s.jpg`;
+    newImage.className = "flickrGallery";
+    flickrGallery.appendChild(newImage);
   });
 };
