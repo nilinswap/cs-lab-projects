1e81c0c5afce9fa213ee06f99fa91afd861babab
Swapnil Sharma <nilinswap@gmail.com>
2023-05-04 15:11:59 +0530

    3: threed break in two functions

--- threedar/final/index.js
+++ threedar/final/index.js
@@ -2,31 +2,37 @@ import * as THREE from "three";
 import { createText } from "./utils.js";
 import { FontLoader } from "three/addons/loaders/FontLoader.js";
 
+let camera, cameraTarget, renderer, scene;
 
-// create canvas and renderer
-const canvas = document.querySelector("#three");
-const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-  
-// create camera
-const fov = 30;
-const aspect = window.innerWidth / window.innerHeight; // the canvas default
-const near = 1;
-const far = 1500;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.set(0, 400, 700);
-const cameraTarget = new THREE.Vector3(0, 150, 0);
+function init() {
+  // create canvas and renderer
+  const canvas = document.querySelector("#three");
+  renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-// create scene, add light
-const scene = new THREE.Scene();
-addLight(scene);
+  // create camera
+  const fov = 30;
+  const aspect = window.innerWidth / window.innerHeight; // the canvas default
+  const near = 1;
+  const far = 1500;
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 400, 700);
+  cameraTarget = new THREE.Vector3(0, 150, 0);
 
-//create mesh, add to scene
-let group = new THREE.Group();
-group.position.y = 100;
-scene.add(group);
-loadFont(group);
+  // create scene, add light
+  scene = new THREE.Scene();
+  addLight(scene);
+
+  //create mesh, add to scene
+  let group = new THREE.Group();
+  group.position.y = 100;
+  scene.add(group);
+  loadFont(group);
+
+  window.addEventListener("resize", onWindowResize);
+
+}
 
 function render() {
   camera.lookAt(cameraTarget);
@@ -43,20 +49,16 @@ function addLight(scene) {
   scene.add(light);
 }
 
-
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
   render();
 }
-  window.addEventListener("resize", onWindowResize);
-
-
 
 function loadFont(group) {
-    let fontName = "optimer", // helvetiker, optimer, gentilis, droid sans, droid serif
-  fontWeight = "bold";
+  let fontName = "optimer", // helvetiker, optimer, gentilis, droid sans, droid serif
+    fontWeight = "bold";
   const loader = new FontLoader();
   loader.load(
     "fonts/" + fontName + "_" + fontWeight + ".typeface.json",
@@ -68,5 +70,5 @@ function loadFont(group) {
   );
 }
 
-
+init();
 render();
