bdf8769ed14b7d084ea9d41b4cd0d5cb1af29da3
Swapnil Sharma <nilinswap@gmail.com>
2023-05-04 16:10:38 +0530

    5: ar text

--- threedar/final/index.js
+++ threedar/final/index.js
@@ -2,41 +2,80 @@ import * as THREE from "three";
 import { createText } from "./utils.js";
 import { FontLoader } from "three/addons/loaders/FontLoader.js";
 
-let camera, cameraTarget, renderer, scene;
+let camera, renderer, scene;
+var markerRoot1;
+var arToolkitSource, arToolkitContext;
 
-function init() {
+function init(font) {
   // create canvas and renderer
-  const canvas = document.querySelector("#three");
-  renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setClearColor(new THREE.Color("lightgrey"), 0);
+  renderer.setSize(640, 480);
+  renderer.domElement.style.position = "absolute";
+  renderer.domElement.style.top = "0px";
+  renderer.domElement.style.left = "0px";
+  document.body.appendChild(renderer.domElement);
 
-  // create camera
-  const fov = 30;
-  const aspect = window.innerWidth / window.innerHeight; // the canvas default
-  const near = 1;
-  const far = 1500;
-  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 400, 700);
-  cameraTarget = new THREE.Vector3(0, 150, 0);
+  
 
   // create scene, add light
   scene = new THREE.Scene();
   addLight(scene);
 
-  //create mesh, add to scene
-  let group = new THREE.Group();
-  group.position.y = 100;
-  scene.add(group);
-  loadFont(group);
+  camera = new THREE.Camera();
+  scene.add(camera);
 
-  window.addEventListener("resize", onWindowResize);
 
+
+  
+
+  arToolkitSource = new THREEx.ArToolkitSource({
+    sourceType: "webcam",
+  });
+
+  // create atToolkitContext
+  arToolkitContext = new THREEx.ArToolkitContext({
+    cameraParametersUrl: "./data/camera_para.dat",
+    detectionMode: "mono",
+  });
+
+  // copy projection matrix to camera when initialization complete
+  arToolkitContext.init(function onCompleted() {
+    camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
+  });
+
+  // build markerControls
+  markerRoot1 = new THREE.Group();
+  let mesh = createText(font, "Swapnil");
+  console.log("mesh", mesh);
+  markerRoot1.add(mesh);
+  scene.add(markerRoot1);
+  let markerControls1 = new THREEx.ArMarkerControls(
+    arToolkitContext,
+    markerRoot1,
+    {
+      type: "pattern",
+      patternUrl: "./data/hiro.patt",
+    }
+  );
+
+  //resize
+  function onResize() {
+    arToolkitSource.onResizeElement();
+    arToolkitSource.copyElementSizeTo(renderer.domElement);
+    if (arToolkitContext.arController !== null) {
+      arToolkitSource.copyElementSizeTo(arToolkitContext.arController.canvas);
+    }
+  }
+  arToolkitSource.init(function onReady() {
+    onResize();
+  });
+  window.addEventListener("resize", function () {
+    onResize();
+  });
 }
 
 function render() {
-  camera.lookAt(cameraTarget);
-  renderer.clear();
   renderer.render(scene, camera);
 }
 
@@ -45,18 +84,15 @@ function addLight(scene) {
   const intensity = 1.5;
   const light = new THREE.PointLight(color, intensity);
   light.color.setHSL(Math.random(), 1, 0.5);
-  light.position.set(0, 100, 90);
+  light.position.set(0, 1, 9);
   scene.add(light);
+
+  let ambientLight = new THREE.AmbientLight(0xcccccc, 0.5);
+  scene.add(ambientLight);
 }
 
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  render();
-}
 
-function loadFont(group) {
+function loadFont() {
   let fontName = "optimer", // helvetiker, optimer, gentilis, droid sans, droid serif
     fontWeight = "bold";
   const loader = new FontLoader();
@@ -64,11 +100,24 @@ function loadFont(group) {
     "fonts/" + fontName + "_" + fontWeight + ".typeface.json",
     function (response) {
       let font = response;
-      group.add(createText(font, "Swapnil"));
-      render();
+      init(font);
+      animate();
     }
   );
 }
 
-init();
-render();
+function update() {
+  if (arToolkitSource.ready !== false)
+    arToolkitContext.update(arToolkitSource.domElement);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  update();
+  render();
+}
+
+loadFont();
+
+
+
--- threedar/final/utils.js
+++ threedar/final/utils.js
@@ -1,14 +1,16 @@
 import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
 import * as THREE from "three";
 
+THREE.Cache.enabled = true;
+
 export function createText(font, text) {
   const materials = [
     new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true }), // front
     new THREE.MeshPhongMaterial({ color: 0xffffff }), // side
   ];
-  const height = 20,
-    size = 70,
-    hover = 30;
+  const height = 1,
+    size = 1,
+    hover = 1;
 
   let textGeo = new TextGeometry(text, {
     font: font,
@@ -16,18 +18,13 @@ export function createText(font, text) {
     height: height,
   });
 
-  textGeo.computeBoundingBox();
-
-  const centerOffset =
-    -0.5 * (textGeo.boundingBox.max.x - textGeo.boundingBox.min.x);
-
+ 
   let textMesh = new THREE.Mesh(textGeo, materials);
 
-  textMesh.position.x = centerOffset;
   textMesh.position.y = hover;
-  textMesh.position.z = 0;
 
-  textMesh.rotation.x = 0;
-  textMesh.rotation.y = Math.PI * 2;
+  textMesh.rotateX(-Math.PI / 2);
   return textMesh;
 }
+
+
